ff6fcb82f344f9466ddc50b6755977af33974bcf
VALAN RAJ S <valanrajs.2364@gmail.com>
2025-05-26 22:32:15 +0530

    Update UART_RX_TOP.v

--- UART_RX/UART_RX_TOP.v
+++ UART_RX/UART_RX_TOP.v
@@ -3,21 +3,22 @@
  	input 			clk		, 
 	input			reset		,
  	input 			rx		,
+	input			p_sel		,
  	output 			baud_tick	, 
  	output  		p_error		,
  	output 		[7:0] 	d_out		,
  	output  		stop_error 	
 );
 
-//wire baud_tick;
 
 uart_rx uut_rx(		
-			.clk	 	(clk	   ), 
-                        .baud_tick      (baud_tick ),
-                        .rx	        (rx	   ),
-                        .p_error        (p_error   ),	  
-                        .d_out_rx       (d_out	   ),
-			.stop_error     (stop_error)
+			.clk	 	(clk		), 
+                        .baud_tick      (baud_tick 	),
+                        .rx	        (rx	   	),
+			.p_sel		(p_sel	        ),
+                        .p_error        (p_error   	),	  
+                        .d_out_rx       (d_out	   	),
+			.stop_error     (stop_error	)
 		);
 
 baud_tick_generator uut_baud(
